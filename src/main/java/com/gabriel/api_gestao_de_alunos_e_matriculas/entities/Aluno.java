97d8ae2a0ab0a47f8199b326eb9e897456636947
Gabriel Sales <gabrielsales07112018@gmail.com>
2025-08-24 13:12:43 -0300

    feat: Adiciona construtores, getter e settes, equals e hashcode na entidade Aluno

--- src/main/java/com/gabriel/api_gestao_de_alunos_e_matriculas/entities/Aluno.java
+++ src/main/java/com/gabriel/api_gestao_de_alunos_e_matriculas/entities/Aluno.java
@@ -1,12 +1,17 @@
 package com.gabriel.api_gestao_de_alunos_e_matriculas.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
 @Table(name = "tb-aluno")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Aluno extends EntidadeBase {
 
     @Id
@@ -19,4 +24,52 @@ public class Aluno extends EntidadeBase {
     @OneToMany(mappedBy = "aluno", cascade = CascadeType.ALL)
     private Set<Matricula> matriculas;
 
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public Set<Matricula> getMatriculas() {
+        return matriculas;
+    }
+
+    public void setMatriculas(Set<Matricula> matriculas) {
+        this.matriculas = matriculas;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Aluno aluno = (Aluno) o;
+        return Objects.equals(id, aluno.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
